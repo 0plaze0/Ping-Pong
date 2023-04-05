c94fe1e9d608470bfadc3c73866a865304dbea8d
plaze <banikakash131@gamil.com>
2023-04-05 17:55:01 +0530

    Ball physics added

---
+++ main.js
@@ -0,0 +1,24 @@
+import Ball from "./Ball.js"
+import Paddle from "./paddle.js"
+
+
+const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+let lastTime;
+const update = (time)=>{
+    if(lastTime != null){
+        const delta = time-lastTime;
+        
+        // ball.update(delta);
+    }
+    lastTime = time;
+    window.requestAnimationFrame(update);
+}
+
+document.addEventListener("mousemove",(event)=>{
+    playerPaddle.position = (event.y/window.innerHeight)*100;
+})
+
+window.requestAnimationFrame(update);//if we can update a frame then run the function
+//update it passed an time agrument from requestAnimationFrame
